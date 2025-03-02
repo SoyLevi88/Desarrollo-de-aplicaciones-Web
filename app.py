fc5b61fd7244ecc747f3842dc6049df8066a1c52
SoyLevi88 <le.mashiantg@uea.edu.ec>
2025-03-02 14:07:15 -0500

    "Nueva Actualizacion 2"

--- app.py
+++ app.py
@@ -1,32 +1,40 @@
-from flask import Flask, request
+from flask import Flask, render_template, redirect , url_for, request, flash
+from flask_bootstraps import Bootstraps
+from flask_wtf import Flaskform
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'tu_clave_secreta'
 
+class MiFormulario(FlaskForm):
+    nombre = StringField('Nombre', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Enviar')
+app = Flask(__name__)
 
+# Página de inicio
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Practica Web nuevamente'
+def index():
+    return render_template('index.html')
 
-#Nueva ruta para mostrar un mensaje personalizado
+# Página "Acerca de"
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Ruta dinámica con nombre de usuario
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
-    return  f'Bienvenido, {nombre}!'
-#Nueva Ruta
-# Ruta para mostrar formulario
-@app.route('/usuario', methods=['GET', 'POST'])
-def usuario_form():
-     if request.method == 'POST':
-         nombre = request.form['nombre']
-         return f' Bienvenido , {nombre}!'
-     return '''
-    , <form method="post">
-         <label for="nombre">Ingrese tu nombre:</label>
-         <input type="text" id="nombre" name="nombre">
-         <button type="submit">Enviar </button>
-     </form>
-     '''
-         
+    return render_template('usuario.html', nombre=nombre)
 
+# Ruta con formulario para ingresar nombre
+@app.route('/usuario/', methods=['GET', 'POST'])
+def usuario_form():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        return render_template('usuario.html', nombre=nombre)
+    return render_template('formulario.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
